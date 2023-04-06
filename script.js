1ace9ca4e0606241d39c62bbf88eec1585aab085
Clansou <clement.souplet78@gmail.com>
2023-04-06 09:14:56 +0200

    Create Check if not empty

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 function getHouse() {
   var name = document.getElementById("name").value;
-  var hash = md5(name);
-  var hashValue = parseInt(hash.substring(0, 8), 16);
-
-  var houseNumber = hashValue % 4 + 1;
-  var house;
-
-  if (houseNumber === 1) {
-    house = "Gryffondor";
-  } else if (houseNumber === 2) {
-    house = "Serpentard";
-  } else if (houseNumber === 3) {
-    house = "Poufsouffle";
-  } else {
-    house = "Serdaigle";
+  if(name != ""){
+    var hash = md5(name);
+    var hashValue = parseInt(hash.substring(0, 8), 16);
+  
+    var houseNumber = hashValue % 4 + 1;
+    var house;
+  
+    if (houseNumber === 1) {
+      house = "Gryffondor";
+    } else if (houseNumber === 2) {
+      house = "Serpentard";
+    } else if (houseNumber === 3) {
+      house = "Poufsouffle";
+    } else {
+      house = "Serdaigle";
+    }
+  
+    var resultElement = document.getElementById("result");
+    resultElement.innerHTML = name + ", vous êtes à " + house + " !";
   }
-
-  var resultElement = document.getElementById("result");
-  resultElement.innerHTML = name + ", vous êtes à " + house + " !";
+  else{
+    var resultElement = document.getElementById("result");
+    resultElement.innerHTML = "Il faut écrire un mot pour savoir dans quel école c'est.";
+  }
+ 
 }
